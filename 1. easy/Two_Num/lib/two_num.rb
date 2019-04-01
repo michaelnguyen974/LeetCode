8fa6cb087c122775eb9b8ccf0f386680656234a8
Michael Nguyen <michaelnguyen974@gmail.com>
2019-04-01 16:02:45 +0100

    refactored: function now works for two inputs and pushes the indexes to an array

--- 1. easy/Two_Num/lib/two_num.rb
+++ 1. easy/Two_Num/lib/two_num.rb
@@ -1,7 +1,14 @@
 class Solution 
 
   def two_num(array_of_nums, target)
-    [0, 1]
+    array = []
+    if array_of_nums.reduce(0){|sum, num| sum + num} === target 
+      array_of_nums.each_with_index do |number, index|
+        array.push(index)
+      end
+    end
+    return array
+
   end
 
 end
