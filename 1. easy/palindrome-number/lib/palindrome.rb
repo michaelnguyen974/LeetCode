f7f2b700dbc9877ea6fef1686c32942e1d60e4af
Michael Nguyen <michaelnguyen974@gmail.com>
2019-04-02 19:38:11 +0100

    solution works for all use cases and edge cases

--- 1. easy/palindrome-number/lib/palindrome.rb
+++ 1. easy/palindrome-number/lib/palindrome.rb
@@ -1,7 +1,12 @@
 class Palindrome
 
   def is_palindrome(number)
-    true
+    input = number.to_s
+    if number.to_s === input.reverse
+      return true 
+    else
+      return false
+    end
   end
 
 end
--- 1. easy/palindrome-number/spec/palindrome_spec.rb
+++ 1. easy/palindrome-number/spec/palindrome_spec.rb
@@ -2,8 +2,18 @@ require 'palindrome.rb'
 
 RSpec.describe Palindrome do 
 
-  it("input of 121 returns true as it is a palindrome") do 
+  it("input of 121 returns true") do 
     palindrome = Palindrome.new 
     expect(palindrome.is_palindrome(121)).to eq(true)
+  end
+
+  it("input of negative number returns false") do 
+    palindrome = Palindrome.new 
+    expect(palindrome.is_palindrome(-121)).to eq(false)
+  end
+
+  it("input short number returns false") do 
+    palindrome = Palindrome.new 
+    expect(palindrome.is_palindrome(10)).to eq(false)
   end
 end 
