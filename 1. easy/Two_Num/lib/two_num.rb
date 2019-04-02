9aeecce80796e18a51dd3c7ff7f0b4e05f446507
Michael Nguyen <michaelnguyen974@gmail.com>
2019-04-02 13:32:10 +0100

    Removed puts statement

--- 1. easy/Two_Num/lib/two_num.rb
+++ 1. easy/Two_Num/lib/two_num.rb
@@ -3,7 +3,6 @@ class Solution
   def two_num(nums, target)
     dict = {}
     nums.each_with_index do |n, i|
-      p dict
       if dict[target - n]  
         return dict[target - n], i
       end
